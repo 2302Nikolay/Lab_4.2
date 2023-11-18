5cfb06677fcffcaba34407eca674d6e2b3781fcc
Мальцев Николай <Malzewn@gmail.com>
2023-11-18 18:29:12 +0300

    Corrected the task from the last lab work

--- Задания/Task_1.py
+++ Задания/Task_1.py
@@ -1,59 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 class Pair:
-    """
-    Класс, хранящий введенные коэффициенты A и B в полях first и second
-    """
 
     def __init__(self, first, second):
-        """
-        Конструктор класса, принимает два параметра, валидирует их и сохраняет в поля
-        """
-        # Проверка, является ли first(A) дробным числом
-        if not isinstance(first, float):
-            raise TypeError("Значение first должно быть дробным числом")
-
-        # Проверка, является ли second(B) дробным числом
-        if not isinstance(second, float):
-            raise TypeError("Значение second должно быть дробным числом")
-
-        # Проверка, не равен ли second(A) нулю
-        if first == 0:
-            raise ValueError("Параметр A не должен быть равен нулю")
-
-        # Записываем значения в поля
         self.first = first
         self.second = second
 
-    def SolLinEqu(self):
-        """
-        Вычисляется значение линейного уравнения, с использованием переданных в конструктор параметров
-        """
-        return (self.second * -1.0)/self.first
-
-    def Display(self):
-        """
-        Метод выводит на консоль линейное уравнение вида Ax+B с подставленными параметрами
-        """
-        print(f"(y = {self.first}x + {self.second})")
-
     @classmethod
-    def Read(cls):
-        """
-        Статичный метод для создания экземпляра класса
-        """
-        a = float(input("Введите коэффициент A: "))
-        b = float(input("Введите коэффициент B: "))
+    def read(cls):
+        line = input("Введите коэффициент А (Пример: 2/3): ")
+        line2 = input("Введите коэффициент В (Пример: 2/3): ")
 
-        return cls(a, b)
+        parts = list(map(int, line.split("/", maxsplit=1)))
+        parts2 = list(map(int, line2.split("/", maxsplit=1)))
+
+        if parts[1] == 0 and parts2[1] == 0:
+            raise ValueError()
+
+        return cls(parts, parts2)
+
+    def display(self):
+        print(f"Y = {self.first[0]}/{self.first[1]}*X + {self.second[0]}/{self.second[1]}")
+
+    def sol_lin_equ(self):
+        return (self.first[1]/self.first[0])*-1.0 / (self.second[1]/self.second[0])
 
 
-if __name__ == '__main__':
-    # Создаем экземпляр класса
-    pair = Pair.Read()
-    # Отображаем уравнение
-    pair.Display()
-    # Выводим посчитанное результат вычисления
-    print(pair.SolLinEqu())
+if __name__ == "__main__":
+    pair = Pair.read()
+    pair.display()
+    print(pair.sol_lin_equ())
